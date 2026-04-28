3170fcb5eab14ee41788b1832d924a6ab07d658f
Fabio Vaz <fabioaugustovaz@outlook.com>
2026-04-28 17:01:20 -0300

    att (funções python)

--- aula2funcao.py
+++ aula2funcao.py
@@ -23,18 +23,58 @@
 # cria_usuario(idade=20, nome="Carol")
 # cria_usuario("Dan", 25, admin=True)
 
-def criar_perfil(nome, idade, cidade):
-    print(f"{nome}, {idade} anos, {cidade}")
+# def criar_perfil(nome, idade, cidade):
+#     print(f"{nome}, {idade} anos, {cidade}")
 
-# Chamadas com argumentos nomeados (ordem não importa)
-criar_perfil(cidade="Curitiba", nome="Chupson", idade=40)
-criar_perfil(nome="Ana", idade=25, cidade="São Paulo")
+# # Chamadas com argumentos nomeados (ordem não importa)
+# criar_perfil(cidade="Curitiba", nome="Chupson", idade=40)
+# criar_perfil(nome="Ana", idade=25, cidade="São Paulo")
 
-# Função com quantidade variável de argumentos
-def somar_tudo(*numeros):
-    return sum(numeros)
+# # Função com quantidade variável de argumentos
+# def somar_tudo(*numeros):
+#     return sum(numeros)
 
-# Exibindo os resultados
-print(somar_tudo(1, 2))
-print(somar_tudo(1, 2, 3, 4))
-print(somar_tudo(20, 30))
+# # Exibindo os resultados
+# print(somar_tudo(1, 2))
+# print(somar_tudo(1, 2, 3, 4))
+# print(somar_tudo(20, 30))
+
+
+
+
+
+# Criando um dicionário 
+
+# produto = {"nome": "Teclado", "preco":150, "em_estoque": True}
+
+# # Acessando um valor 
+# print(produto["nome"])
+
+# # Modificando um valor
+# produto["preco"] = 135.00
+
+# # Adicionando um novo par 
+# produto["quantidade"] = 10
+
+# print(produto)
+
+
+
+def exibir_info(**dados):
+    for chave, valor in dados.items():
+        print(f"{chave}: {valor}")
+    
+# Criamos um dicionário vazio para armazenar as entradas
+info_usuario = {}
+
+print("Digite as informações (ou 'sair' na chave para encerrar): ")
+
+while True:
+    chave = input("Nome do campo (ex: Profissão): ")
+    if chave.lower() == 'sair':  # corrigido aqui
+        break 
+    valor = input(f"Valor para {chave}: ")
+    info_usuario[chave] = valor
+
+# Usamos ** para desempacotar o dicionário com argumentos 
+exibir_info(**info_usuario)
