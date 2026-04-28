28903aeeb8b3900cf8f1ad008133d1fd1ca42f24
Fabio Vaz <fabioaugustovaz@outlook.com>
2026-04-28 15:53:20 -0300

    att

--- aula2funcao.py
+++ aula2funcao.py
@@ -10,15 +10,23 @@
 # print(formatar_real_replace(preco)) # R$ 1.234,50
 
 # Parametros com valor padrão (default)
-def saudacao(nome, mensagem="Bem vindo!"):
-    print(f"Olá, {nome}! {mensagem}")
+# def saudacao(nome, mensagem="Bem vindo!"):
+#     print(f"Olá, {nome}! {mensagem}")
 
-saudacao("Ana")
-saudacao("Bob", "Bom dia!")
+# saudacao("Ana")
+# saudacao("Bob", "Bom dia!")
 
-# Argumentos nomeados (keyword args)
-def cria_usuario(nome, idade, admin=False):
-    print(f"{nome} | {idade} anos | admin = {admin}")
+# # Argumentos nomeados (keyword args)
+# def cria_usuario(nome, idade, admin=False):
+#     print(f"{nome} | {idade} anos | admin = {admin}")
 
-cria_usuario(idade=20, nome="Carol")
-cria_usuario("Dan", 25, admin=True)
+# cria_usuario(idade=20, nome="Carol")
+# cria_usuario("Dan", 25, admin=True)
+
+def criar_perfil(nome, idade, cidade):
+    print(f'{nome}, {idade} anos ,{cidade}')
+
+criar_perfil(cidade="Curitiba", nome="Chupson", nome="40")
+#Chupson, 40 anos, Curitiba - funciona independente da ordem!
+
+
